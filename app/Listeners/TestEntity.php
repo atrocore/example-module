f74b6c6d816ae4a9d0f1e33cff5d0bc9cdb2c306
r.ratsun <33658723+rratsun@users.noreply.github.com>
2023-09-13 09:14:44 +0300

    Update TestEntity.php

--- app/Listeners/TestEntity.php
+++ app/Listeners/TestEntity.php
@@ -3,11 +3,17 @@
 namespace ExampleModule\Listeners;
 
 use Atro\Listeners\AbstractListener;
+use Atro\Core\EventManager\Event;
 
 /**
  * Test Entity Listener
  */
 class TestEntity extends AbstractListener
 {
+  public function beforeSave(Event $event)
+    {
+        $entity = $event->getArgument('entity');
 
+      // do something here
+    }
 }
